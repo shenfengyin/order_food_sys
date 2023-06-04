c7c18ffb28829c5b916f047bf48a143865eeb93e
sfy <1923554655@qq.com>
2023-06-05 00:40:51 +0800

    完善移动端的订单查询

--- reggie_take_out/src/main/java/com/itheima/reggie/controller/OrderController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -1,14 +1,16 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.entity.Dish;
 import com.itheima.reggie.entity.Orders;
 import com.itheima.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 订单
@@ -31,5 +33,30 @@ public class OrderController {
         log.info("订单数据：{}",orders);
         orderService.submit(orders);
         return R.success("下单成功");
+    }
+
+    /**
+    * Description:用户查询订单数据
+    * date: 2023/6/5 0:26
+    * @author: sfy
+    */
+    @GetMapping("/userPage")
+    public  R<Page> userPage(int page, int pageSize) {
+        log.info("查询订单数据成功");
+
+        //构造分页构造器对象
+        Page<Orders> pageInfo = new Page<>(page,pageSize);
+//        Page<DishDto> dishDtoPage = new Page<>();
+
+        //条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Orders::getUserId, BaseContext.getCurrentId())
+                .orderByDesc(Orders::getOrderTime);
+
+        //分页查询
+        orderService.page(pageInfo, queryWrapper);
+
+
+        return R.success(pageInfo);
     }
 }
