361634ef06d9a2fdead6bd19db2fade134847033
sfy <1923554655@qq.com>
2023-06-12 01:49:22 +0800

    lua脚本实现'判断+释放'原子操作

--- reggie_take_out/src/main/java/com/itheima/reggie/lock/SimpleRedisLock.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/lock/SimpleRedisLock.java
@@ -1,8 +1,13 @@
 package com.itheima.reggie.lock;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+
+import java.util.Collection;
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -25,6 +30,12 @@ public class SimpleRedisLock implements ILock{
     //每台JVM的ID_pre不一样
     //相同的JVM有线程号区分，不同的JVM有uuid区分
     private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
 
     @Override
@@ -37,17 +48,32 @@ public class SimpleRedisLock implements ILock{
         return Boolean.TRUE.equals(success);
     }
 
-
-    //释放锁-解决误删问题(根据业务锁名K，判断v是否一样（即是不是自己的锁
     @Override
     public void unlock() {
-        //获取线程标识和锁的标识是否一致
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = (String) redisTemplate.opsForValue().get("lock:" + name);
-
-        if(threadId.equals(id)) {
-            redisTemplate.delete("lock:" + name);
-        }
-
+        //调用lua脚本-实现-保证“判断+释放”是原子操作
+        redisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList("lock:" + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+
+    //释放锁-解决误删问题(根据业务锁名K，判断v是否一样（即是不是自己的锁
+
+
+
+
+//    @Override
+//    public void unlock() {
+//        //获取线程标识和锁的标识是否一致
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        String id = (String) redisTemplate.opsForValue().get("lock:" + name);
+//        //极端情况下，刚好判断完再释放的时候：
+//        //此时线程1业务逻辑阻塞了，线程1的锁超时释放了；线程2进来获取到锁；然后线程1业务逻辑继续，删除了锁
+//        //因此要改进：保证“判断+释放”是原子操作
+//        if(threadId.equals(id)) {
+//            redisTemplate.delete("lock:" + name);
+//        }
+//
+//    }
 }
